8b233d83b23f58a55d4867560ba12d927fc93f8e
Daniel <acidistrict@gmail.com>
2022-09-27 12:56:37 +0100

    add antd button and table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import { Button, Table } from "antd";
 
-function App() {
-  return <div>Install antd library</div>;
-}
+const App = () => {
+  const data = [
+    { key: "1", name: "Frank", age: 24, location: "London" },
+    { key: "2", name: "Mike", age: 28, location: "Amsterdam" },
+    { key: "3", name: "Martha", age: 31, location: "Oslo" },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Location",
+      dataIndex: "location",
+      key: "location",
+    },
+  ];
+
+  return (
+    <div>
+      <Table dataSource={data} columns={columns} />
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
 
 export default App;
